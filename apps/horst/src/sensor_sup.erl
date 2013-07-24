b8f6726bdc414f1ebb0568a7e818c106fc3c1546
Ulf <uaforum1@googlemail.com>
2013-07-24 11:01:20 +0200

    now i can configure which sensor i will load on startup

--- apps/horst/src/sensor_sup.erl
+++ apps/horst/src/sensor_sup.erl
@@ -21,16 +21,43 @@
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 -define(CHILD_1(I, Type, Args), {I, {I, start_link, [Args]}, permanent, 5000, Type, [I]}).
+-define(CONFIG_FILE, "sensor.config").
 %% ===================================================================
 %% API functions
 %% ===================================================================
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+start_child(Module) ->
+	io:format("Modul : ~p~n", [?CHILD(Module, worker)]),
+    supervisor:start_child(Module, ?CHILD(Module, worker)).
+
+create_child_specs() ->
+	Config = get_config(),
+	[?CHILD(Sensor, worker) || {Sensor} <- Config].
+
 init([]) ->
-    {ok, {{one_for_one, 1, 10000},[
-    								?CHILD(hc_sr501_sensor, worker), 
-    							   	?CHILD(dht22_sensor, worker)
-    							  ]}}.
+    {ok, {{one_for_one, 1, 10000}, create_child_specs()}}.
+
 get_sensors() ->
 	supervisor:which_children(?MODULE).
+
+get_config() ->
+	{ok, Config} = file:consult(filename:join([code:priv_dir(horst),"config", ?CONFIG_FILE])),
+	Config.
+	
+-include_lib("eunit/include/eunit.hrl").
+
+-ifdef(TEST).
+
+get_config_test() ->
+	application:load(horst),
+	Config = get_config(),
+	%%	?debugFmt("Config File : ~p", [Config]),
+	?assertEqual([{hc_sr501_sensor}, {dht22_sensor}], Config).
+
+
+start_child_test() ->
+	application:load(horst),
+	?assertEqual([?CHILD(hc_sr501_sensor, worker), ?CHILD(dht22_sensor, worker)], create_child_specs()).
+-endif.
