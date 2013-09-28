0c9be18e3039855361978615e77f9a4e1411f2f4
Ulf <uaforum1@googlemail.com>
2013-09-28 06:23:17 +0200

    shortcut for sending messages

--- apps/horst/src/sensor.erl
+++ apps/horst/src/sensor.erl
@@ -15,7 +15,7 @@
 %% --------------------------------------------------------------------
 %% External exports
 %% --------------------------------------------------------------------
--export([create_message/5, create_message/4, create_message/3, send_message/2]).
+-export([create_message/5, create_message/4, create_message/3, send_message/2, send_message/1]).
 -export([get_id/1]).
 -export([generate_messages/1, generate_messages/2]).
 %% --------------------------------------------------------------------
@@ -31,6 +31,8 @@ create_message(Node, Module, Id, Body) ->
 create_message(Node, Module, Id, Time, Body) ->
     [atom_to_binary(Node, utf8), atom_to_binary(Module, utf8), list_to_binary(Id), list_to_binary(integer_to_list(Time)), Body].
 
+send_message(Message) ->
+	send_message(nodes(), Message).
 send_message(Nodes, Message) ->
 	send_message(Nodes, 'actor_group', Message).    
 send_message(Nodes, Target, Message) ->
