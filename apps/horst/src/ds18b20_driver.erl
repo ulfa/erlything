76f511a10d00008064e0e649bb3ff677531756f6
Ulf <uaforum1@googlemail.com>
2013-10-05 04:07:53 +0200

    added ds18b20 thing

---
+++ apps/horst/src/ds18b20_driver.erl
@@ -0,0 +1,56 @@
+%% Copyright (c) 2013 Ulf Angermann
+%% See MIT-LICENSE for licensing information.
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf Angermann uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created :  
+-module(ds18b20_driver).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([init/1, call_sensor/1]).
+
+init(Config) ->
+    lager:info("ds18b20_driver:init('~p')", [Config]),  
+    os:cmd("modprobe w1-gpio"),
+    os:cmd("modprobe w1-therm").
+
+call_sensor(Config) ->
+    Temp_line = call_driver(),
+    Msg = sensor:create_message(node(), ?MODULE, sensor:get_id(Config), parse_message(Temp_line)),
+    sensor:send_message(Msg),
+    Config.
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+call_driver() ->
+  [W1_Slave] = filelib:wildcard("/sys/bus/w1/devices/28*/w1_slave"),   
+  {ok, File} = file:open(W1_Slave, [read]),
+  {ok, Yes_line} = file:read_line(File),
+  timer:sleep(200), 
+  {ok, Temp_line} = file:read_line(File),
+  file:close(File),
+  Temp_line.   
+
+parse_message(Msg) ->
+    lager:info("Msg from ds18b20 : ~p", [Msg]),
+    case re:run(Msg ,"t=([0-9.]+)") of 
+       nomatch -> {temp, 0.0};
+       {match,[{C1,C2},{C3,C4}]} -> {temp, erlang:list_to_integer(string:substr(Msg, C3 + 1, C4))/1000}
+    end.
+    
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+parse_message_test() ->
+    Msg = "37 01 4b 46 7f ff 09 10 26 : crc=26 YES\n 37 01 4b 46 7f ff 09 10 26 t=19437\n",
+    ?assertEqual({temp, "19437"}, parse_message(Msg)).
+-endif.
