96181ee81f0ec1b29d9fcbef4d29e2aa4033de47
Ulf <uaforum1@googlemail.com>
2013-10-19 06:01:51 +0200

    is the device which will be switched the right one

--- apps/horst/src/transmitter_433_driver.erl
+++ apps/horst/src/transmitter_433_driver.erl
@@ -18,12 +18,16 @@
 -export([handle_msg/3]).
 
 handle_msg([Node ,Sensor, _Id, Time, {Switch, Number, Status}], Config, Module_config) ->
-	call_driver(Number, Status),
-	Table_Id = proplists:get_value(?TABLE, Config),
-	Data = ets:tab2list(Table_Id), 	
-	Data1 = lists:keyreplace(Switch, 1 , Data, {Switch, Number, Status}),
-	ets:insert(Table_Id, Data1),
-	Config;
+	case is_valid_device(Number, Module_config) of 
+		false -> lager:info("Device : ~p is not a valid device for this node", [Number]),
+				 Config;
+		_ -> call_driver(Number, Status),
+			 Table_Id = proplists:get_value(?TABLE, Config),
+			 Data = ets:tab2list(Table_Id), 	
+			 Data1 = lists:keyreplace(Switch, 1 , Data, {Switch, Number, Status}),
+			 ets:insert(Table_Id, Data1),
+			 Config
+	end;
 
 handle_msg([Node ,Sensor, _Id, Time, Body], Config, Module_config) ->
 	lager:warning("transmitter_433_driver got the wrong message : ~p", [[Node ,Sensor, _Id, Time, Body]]),
@@ -31,6 +35,9 @@ handle_msg([Node ,Sensor, _Id, Time, Body], Config, Module_config) ->
 %% --------------------------------------------------------------------
 %%% Internal functions
 %% --------------------------------------------------------------------
+is_valid_device(Number, Module_config) ->
+	lists:keysearch(Number, 2, Module_config).
+
 call_driver(Switch, Status) ->
 	lager:debug("switch state from : ~p to : ~p", [Switch, Status]),
     Driver = filename:join([code:priv_dir(horst), "driver", "remote", "send "]),
