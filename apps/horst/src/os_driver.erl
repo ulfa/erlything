0b737fb27de24e05b009b5001e9845d1c350d8d9
Ulf <uaforum1@googlemail.com>
2013-11-01 06:49:16 +0100

    added a os_sensor

---
+++ apps/horst/src/os_driver.erl
@@ -0,0 +1,59 @@
+%% Copyright (c) 2013 Ulf Angermann
+%% See MIT-LICENSE for licensing information.
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf Angermann uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created :  
+-module(os_driver).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include("../include/horst.hrl").
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([init/1, stop/1]).
+-export([call_sensor/2]).
+
+
+init(Config) ->
+    application:start(os_mon),
+    Config.
+
+stop(Config) ->
+    lager:info("stop is called"),
+    application:stop(os_mon),
+    application:unload(os_mon), 
+    Config.    
+
+call_sensor(Config, Module_config) ->
+    Data = call_driver(),
+    Msg = sensor:create_message(node(), ?MODULE, sensor:get_id(Config), date:get_date_seconds(), {Data}),
+    sensor:send_message(nodes(), Msg),
+    thing:save_data_to_ets(Config, Data),
+    Config.
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+call_driver() ->
+    [{temp, read_temp()}, {avg1, divide(cpu_sup:avg1())}, {avg5, divide(cpu_sup:avg5())}, {avg15, divide(cpu_sup:avg15())}].
+
+read_temp() ->
+    {ok, T} = file:read_file("/sys/class/thermal/thermal_zone0/temp"),
+    Temp = erlang:list_to_integer(string:strip(binary_to_list(T), right, $\n)) / 1000.
+
+divide(Value) ->
+    Value/256.
+
+
+
+%%" 17:56:58 up  6:34,  3 users,  load average: 0.76, 0.78,"
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+-endif.
