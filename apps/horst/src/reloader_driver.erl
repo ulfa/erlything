c6f8e214a379e5944d2c6553a605f5e0879d0498
Ulf <uaforum1@googlemail.com>
2013-11-28 07:50:27 +0100

    fixed an issue

---
+++ apps/horst/src/reloader_driver.erl
@@ -0,0 +1,48 @@
+%% Copyright (c) 2013 Ulf Angermann
+%% See MIT-LICENSE for licensing information.
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf Angermann uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created : 14.11.2013
+-module(reloader_driver).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include("../include/horst.hrl").
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([call_sensor/2]).
+-export([init/1, stop/1]).
+
+init(Config) ->
+    application:start(reloader).
+
+stop(Config) ->
+    application:stop(reloader),
+    application:unload(reloader).  
+
+call_sensor(Config, Module_config) ->   
+    Changed_Modules = code_reloader:reload(),
+    send_message(Config, Changed_Modules),
+    Config.
+
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+send_message(Config, []) ->
+    lager:debug("there are no modules which i could reload.");
+
+send_message(Config, Changed_Modules) ->
+    Msg = sensor:create_message(node(), ?MODULE, sensor:get_id(Config), Changed_Modules), 
+    sensor:send_message(Msg).
+
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+-endif.
