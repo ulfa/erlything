25288715e2417efb8e0c45aed2840e397e126ef4
Ulf <uaforum1@googlemail.com>
2013-12-17 08:12:52 +0100

    added ds18b20 resource

--- apps/leni/src/ds18b20_resource.erl
+++ apps/leni/src/ds18b20_resource.erl
@@ -235,8 +235,7 @@ get_data(Node, Name) when is_list(Node)->
     case rpc:call(list_to_atom(Node), thing, get_module_config, [Name]) of 
         {badrpc, Reason} -> lager:error("got error during call ~p thing:get_driver(~p) with reason ~p", [Node, Name, Reason]),
                             [];
-        Data -> {temp, Temp} = proplists:get_value(data, Data, []),
-                Temp
+        [{data, Data}]     ->  convert_timestamp_to_date(Data)
     end.
 
 convert_timestamp_to_date(List_of_temps) ->
