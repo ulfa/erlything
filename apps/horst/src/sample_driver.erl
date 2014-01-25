d6dba97bf35b6b8545d19f772bde8ed5c6b24f85
Ulf <uaforum1@googlemail.com>
2014-01-25 06:17:39 +0100

    added sensor sample

---
+++ apps/horst/src/sample_driver.erl
@@ -0,0 +1,32 @@
+%% Copyright (c) 2013 Ulf Angermann
+%% See MIT-LICENSE for licensing information.
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf Angermann uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created :  
+-module(sample_driver).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include("../include/horst.hrl").
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([call_sensor/2]).
+
+call_sensor(Config, Module_config) ->
+    Msg = sensor:create_message(node(), ?MODULE, [{test_message}]),
+    sensor:send_message(Msg),
+    Config.
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+-endif.
