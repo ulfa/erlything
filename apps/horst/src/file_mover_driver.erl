00a998dead3af6045c0231636d9d2bb5e875995b
Ulf <uaforum1@googlemail.com>
2014-03-22 04:17:50 +0100

    start the file_mover thing

---
+++ apps/horst/src/file_mover_driver.erl
@@ -0,0 +1,55 @@
+%% Copyright (c) 2013 Ulf Angermann
+%% See MIT-LICENSE for licensing information.
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf Angermann uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created :  
+-module(file_mover_driver).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include("../include/horst.hrl").
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([init/1, call_sensor/2]).
+
+init(Config) ->
+    Module_config = config:get_module_config(Config),
+    Destination = config:get_value(destination, Module_config), 
+    create_destination(Destination),
+    {ok, Config}.
+
+call_sensor(Config, Module_config) ->
+    [Filter, Source, Destination] = config:get_level_values([data], [file_format, source, destination], Module_config), 
+    move_files(Destination, find_files(Source, Filter)),
+    Config.
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+move_files(Destination, []) ->
+    lager:info("move file! "),
+    ok;
+move_files(Destination, [File|List_of_files]) ->
+    lager:info("move file : ~p to dest : ~p", [File, Destination]),
+    {ok, BytesCopied} = file:copy(File, file:join(Destination, filename:basename(File))),
+    ok = file:delete(File), 
+    move_files(Destination, List_of_files).
+
+find_files(Path, Filter) ->
+    filelib:wildcard(filename:join(Path, Filter)). 
+
+create_destination(Destination) ->
+    case filelib:is_dir(Destination) of 
+        true -> ok;
+        false -> file:make_dir(Destination)
+    end.  
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+-endif.
