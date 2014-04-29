b255ec7b7e2aac342552f018e4ff79d6c1dc494d
Ulf <uaforum1@googlemail.com>
2014-04-29 06:50:47 +0200

    added the growler interface thing

---
+++ apps/horst/src/growler_driver.erl
@@ -0,0 +1,53 @@
+%% Copyright (c) 2013 Ulf Angermann
+%% See MIT-LICENSE for licensing information.
+
+%%% -------------------------------------------------------------------
+%%% Author  : Ulf Angermann uaforum1@googlemail.com
+%%% Description :
+%%%
+%%% Created :  
+-module(growler_driver).
+
+%% --------------------------------------------------------------------
+%% defines
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include("../include/horst.hrl").
+%% --------------------------------------------------------------------
+%% External exports
+%% --------------------------------------------------------------------
+-export([init/1, handle_msg/3]).
+
+init(Config) ->
+    growler_sup:start_link(),
+    {ok, Config}.
+
+handle_msg([Node ,<<"system">>, Id, Time, {error,{dead, Node1}}], Config, Module_config) -> 
+    send_message(error, io_lib:format("Node: ~p is dead!", [Node1])),
+    Config;   
+
+handle_msg([Node ,Sensor, Id, Time, {error,{Config_file, Text, Reason}}], Config, Module_config) -> 
+    send_message(error, io_lib:format("Config: ~p is corrupt, because of : ~p", [Config_file, Reason])),
+    Config;   
+
+
+handle_msg(Unknown_message, Config, Module_config) ->
+    lager:warning("~p got a message with incorrect values: ~p",[?MODULE, Unknown_message]),
+    Config.
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+send_message(error, Message) ->
+    growler:error(Message);
+send_message(info, Message) ->
+    growler:success(Message). 
+
+%% --------------------------------------------------------------------
+%%% Test functions
+%% --------------------------------------------------------------------
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+-endif.
