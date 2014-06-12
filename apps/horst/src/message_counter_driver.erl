39686fb8897d265cdfa68595afb841460d6c1d11
Ulf <uaforum1@googlemail.com>
2014-06-12 05:21:29 +0200

    removed the duplicate time info

--- apps/horst/src/message_counter_driver.erl
+++ apps/horst/src/message_counter_driver.erl
@@ -22,7 +22,7 @@ handle_msg([Node ,Sensor, Id, Time, Body], Config, Module_config) ->
 	ets:update_counter(Table_Id, counter, 1),
 	[{data, Data}] = ets:lookup(Table_Id, data),
 	ets:insert(Table_Id, [{data, add(Data, {date:timestamp_to_date(Time), [binary_to_list(Node),
-		binary_to_list(Sensor), binary_to_list(Id), date:timestamp_to_date(Time), Body]})}]), 
+		binary_to_list(Sensor), binary_to_list(Id), Body]})}]), 
 	Config.
 %% --------------------------------------------------------------------
 %%% Internal functions
