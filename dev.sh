4b6bd00f9119bfded74a5d45a65273ede6472f3c
Ulf <uaforum1@googlemail.com>
2015-02-25 06:43:30 +0100

    changed structure of the config file

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p log
 #erl -sname erlything -setcookie nocookie -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -boot start_sasl -s lager -s erlbuild -s erlything -config erlything.config
-erl -sname erlything -setcookie nocookie -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -boot start_sasl -mnesia dir data -s erlbuild -s erlything -config erlything.config
+erl -sname erlything -setcookie nocookie -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -boot start_sasl -mnesia dir data -s erlbuild -s erlything -config etc/app.config
 
--- dev1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir -p log
-erl -sname erlything1 -setcookie nocookie -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -boot start_sasl -s lager -s roni -s horst
