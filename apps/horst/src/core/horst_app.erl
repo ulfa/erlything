b481a7f629b4ae67e3cc80b85883b1c60bed70c2
Ulf Angermann <ua@innoq.com>
2015-05-04 12:55:55 +0200

    refactored mnesia handling

--- apps/horst/src/core/horst_app.erl
+++ apps/horst/src/core/horst_app.erl
@@ -11,7 +11,15 @@
 
 
 start(_StartType, _StartArgs) ->
+	start_database(),
     horst_sup:start_link().
 
 stop(_State) ->
     ok.
+
+start_database() ->
+    lager:info("create the schema for the database"),
+    application:stop(mnesia),    
+    mnesia:create_schema([node()]),
+    lager:info("start the database"),
+    application:start(mnesia).
