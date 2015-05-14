9dc4a95794e274b1abeadc744bf161a916d60f29
Ulf Angermann <ua@innoq.com>
2015-05-14 06:55:24 +0200

    refactoring

--- prod.sh
+++ prod.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p log
-erl -sname erlything -setcookie nocookie -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -boot start_sasl -mnesia dir data -s erlbuild -s erlything -config etc/app.config -detached
+erl -sname erlything -setcookie nocookie -pa $PWD/apps/*/ebin $PWD/deps/*/ebin -boot start_sasl -mnesia dir data -s erlything -config etc/app.config -detached
